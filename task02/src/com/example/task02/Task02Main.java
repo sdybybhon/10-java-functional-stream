78f3f16d77123644036b80021e40fe42915eb3dc
sdybybhon <artem563732@mail.ru>
2024-11-29 09:55:41 +0500

    Уфимцев Артём. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,19 +5,18 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
-        cycleGrayCode(2)
-                .limit(10)
+        cycleGrayCode(3)
+                .limit(50)
                 .forEach(System.out::println);
-        */
-
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException("Неверное значение \"n\". Значение должно быть от 1 до 16 включительно.");
+        }
 
-        return null; // your implementation here
-
+        int size = 1 << n;
+        return IntStream.iterate(0, i -> (i + 1) % size)
+                .map(i -> i ^ (i >> 1));
     }
-
 }
